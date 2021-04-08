edbb0feedb4400665b37b9d457f3bad5981783e5
Russell Kerber <russell.kerber@gmail.com>
2021-04-08 17:33:25 -0600

    modal backgrounds are all dimmed

--- components/DeleteFavorite.js
+++ components/DeleteFavorite.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet, Button, Platform, TouchableOpacity, ImageBackground } from 'react-native';
 import Modal from 'react-native-modal';
-import AsyncStorage from '@react-native-community/async-storage';
 import colors from '../assets/colors';
-
 import { connect } from 'react-redux';
 
-const DeleteFavorite = props => {
+DeleteFavorite = props => {
 
     deleteFavorite = () => {
         console.log('in delete function');
@@ -17,84 +15,137 @@ const DeleteFavorite = props => {
         props.returnToCameraPage()
     }
 
-        return (
-            <Modal visible={props.visible} animationType='slide' transparent={true} hasBackdrop={true} backdropColor={'black'} backdropOpacity={0.70}>
-                <View style={{
-                    flex:1, 
-                    alignItems: 'center', 
-                    justifyContent: 'center', 
-                    marginLeft:'5%', 
-                    marginRight:'5%', 
-                    marginTop:'40%', 
-                    marginBottom:'40%', 
-                    backgroundColor:colors.cream, 
-                    borderWidth:0, 
-                    borderColor:'black', 
-                    borderRadius:10, 
-                    paddingLeft:'5%', 
-                    paddingRight:'5%'
+    return (
+        <Modal isVisible={props.visible} animationIn='slideInUp' animationOut='slideOutDown'>
+            <View style={{
+                flex:1, 
+                alignItems: 'center', 
+                justifyContent: 'center', 
+                marginLeft:'5%', 
+                marginRight:'5%', 
+                marginTop:'40%', 
+                marginBottom:'40%', 
+                backgroundColor:colors.cream, 
+                borderWidth:0, 
+                borderColor:'black', 
+                borderRadius:10, 
+                paddingLeft:'5%', 
+                paddingRight:'5%'
+            }}>
+                <Text style={{
+                    fontSize:36, 
+                    textAlign:'center',
+                    fontFamily: 'Rubik-Regular'
                 }}>
-                    <Text style={{
-                        fontSize:36, 
-                        textAlign:'center',
-                        fontFamily: 'Rubik-Regular'
+                    Delete image?
+                </Text>
+                <Text style={{
+                    fontSize:20, 
+                    textAlign:'center', 
+                    marginTop:'10%',
+                    fontFamily: 'Rubik-Regular'
+                }}>
+                    The image will be permanently deleted.
+                </Text>
+                <TouchableOpacity 
+                    onPress={deleteFavorite} 
+                    style={{ 
+                        width: '75%',
+                        height: 40,
+                        borderWidth: 0,
+                        borderColor: 'black',
+                        borderRadius: 10,
+                        backgroundColor: colors.red,
+                        justifyContent: 'center',
+                        marginTop: '12%'
                     }}>
-                        Delete image?
+                    <Text
+                        style={{
+                            fontSize: 26,
+                            textAlign: 'center',
+                            fontFamily: 'Rubik-Regular'
+                        }}>
+                        Delete
                     </Text>
-                    <Text style={{
-                        fontSize:20, 
-                        textAlign:'center', 
-                        marginTop:'10%',
-                        fontFamily: 'Rubik-Regular'
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    onPress={props.closeDeleteFavoriteModal} 
+                    style={{ 
+                        width: '75%',
+                        height: 40,
+                        borderWidth: 0,
+                        borderColor: 'black',
+                        borderRadius: 10,
+                        backgroundColor: colors.blue,
+                        justifyContent: 'center',
+                        marginTop: 10
                     }}>
-                        The image will be permanently deleted.
+                    <Text
+                        style={{
+                            fontSize: 26,
+                            textAlign: 'center',
+                            fontFamily: 'Rubik-Regular'
+                        }}>
+                        Cancel
                     </Text>
-                    <TouchableOpacity 
-                        onPress={deleteFavorite} 
-                        style={{ 
-                            width: '75%',
-                            height: 40,
-                            borderWidth: 0,
-                            borderColor: 'black',
-                            borderRadius: 10,
-                            backgroundColor: colors.red,
-                            justifyContent: 'center',
-                            marginTop: '12%'
-                        }}>
-                        <Text
-                            style={{
-                                fontSize: 26,
-                                textAlign: 'center',
-                                fontFamily: 'Rubik-Regular'
-                            }}>
-                            Delete
-                        </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity 
-                        onPress={props.closeDeleteFavoriteModal} 
-                        style={{ 
-                            width: '75%',
-                            height: 40,
-                            borderWidth: 0,
-                            borderColor: 'black',
-                            borderRadius: 10,
-                            backgroundColor: colors.blue,
-                            justifyContent: 'center',
-                            marginTop: 10
-                        }}>
-                        <Text
-                            style={{
-                                fontSize: 26,
-                                textAlign: 'center',
-                                fontFamily: 'Rubik-Regular'
-                            }}>
-                            Cancel
-                        </Text>
-                    </TouchableOpacity>                    
-                </View>  
-            </Modal>
-        )
+                </TouchableOpacity>                    
+            </View>  
+        </Modal>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1, 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        marginLeft: '5%', 
+        marginRight: '5%', 
+        marginTop: '40%', 
+        marginBottom: '40%', 
+        backgroundColor: colors.cream, 
+        borderWidth: 0, 
+        borderColor: 'black', 
+        borderRadius: 10, 
+        paddingLeft: '5%', 
+        paddingRight: '5%'
+    },
+    text: {
+        fontSize: 36, 
+        textAlign: 'center', 
+        fontFamily: 'Rubik-Regular'
+    },
+    yesButton: {
+        width: '75%',
+        height: 40,
+        borderWidth: 0,
+        borderColor: 'black',
+        borderRadius: 10,
+        backgroundColor: colors.blue,
+        justifyContent: 'center',
+        marginTop: '20%',
+        alignItems: 'center'
+    },
+    yesButtonText: {
+        fontSize: 26,
+        fontFamily: 'Rubik-Regular'
+    },
+    cancelButton: { 
+        width: '75%',
+        height: 40,
+        borderWidth: 0,
+        borderColor: 'black',
+        borderRadius: 10,
+        backgroundColor: colors.red,
+        justifyContent: 'center',
+        marginTop: 10,
+        alignItems: 'center'
+    },
+    cancelButtonText: {
+        fontSize: 26,
+        fontFamily: 'Rubik-Regular'
+    }
+})
 
 const mapReduxStateToProps = reduxState => ({
     reduxState
--- components/Report.js
+++ components/Report.js
@@ -1,120 +1,153 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet, Button, Platform, TouchableOpacity, ImageBackground, Modal } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import { View, Text, TextInput, StyleSheet, Button, Platform, TouchableOpacity, ImageBackground } from 'react-native';
+import Modal from 'react-native-modal';
 import colors from '../assets/colors';
-
 import { connect } from 'react-redux';
 
-class Report extends React.Component {
+Report = props => {
 
-    state = {
-        accessToken: ''
+    deleteFavorite = () => {
+        console.log('in delete function');
+        //actually delete image
+        props.dispatch({
+            type: 'DELETE_FAVORITE'
+        })
+        props.returnToCameraPage()
     }
 
-    getToken = async () => {
-        try {
-            const token = await AsyncStorage.getItem("access_token")
-            console.log('getToken token:', token);
-            return token;
-        } catch (error) {
-            console.log('AsyncStorage retrieval error:', error.message);
-        }
-        return '(missing token)';
-    }
-
-    async componentDidMount() {
-        console.log('in Report componentDidMount')
-        await this.getToken()
-            .then(response => {
-                //console.log('in new .then. token:', response)
-                this.setState({accessToken: response});
-            }).catch(error => {
-                console.log('in catch,', error)
-            });
-    }
-
-    render() {
-        return (
-            <Modal visible={this.props.visible} animationType='slide' transparent={true}>
-                <View style={{
-                    flex:1, 
-                    alignItems: 'center', 
-                    justifyContent: 'center', 
-                    marginLeft:'5%', 
-                    marginRight:'5%', 
-                    marginTop:'40%', 
-                    marginBottom:'40%', 
-                    backgroundColor:colors.cream, 
-                    borderWidth:0, 
-                    borderColor:'black', 
-                    borderRadius:10, 
-                    paddingLeft:'5%', 
-                    paddingRight:'5%'
-                }}>
-                        <Text style={{
-                            fontSize:36, 
-                            textAlign:'center', 
-                            fontFamily: 'Rubik-Regular'
+    return (
+        <Modal isVisible={props.visible} animationIn='slideInUp' animationOut='slideOutDown'>
+            <View style={{
+                flex:1, 
+                alignItems: 'center', 
+                justifyContent: 'center', 
+                marginLeft:'5%', 
+                marginRight:'5%', 
+                marginTop:'40%', 
+                marginBottom:'40%', 
+                backgroundColor:colors.cream, 
+                borderWidth:0, 
+                borderColor:'black', 
+                borderRadius:10, 
+                paddingLeft:'5%', 
+                paddingRight:'5%'
+            }}>
+                    <Text style={{
+                        fontSize:36, 
+                        textAlign:'center', 
+                        fontFamily: 'Rubik-Regular'
+                    }}>
+                        Bad Vibes?
+                    </Text>
+                    <Text style={{
+                        fontSize:20, 
+                        textAlign:'center', 
+                        marginTop:'10%',
+                        fontFamily: 'Rubik-Regular'
+                    }}>
+                            The sender will be temporarily banned.
+                    </Text>
+                    <TouchableOpacity 
+                        onPress={() => props.report()} 
+                        style={{ 
+                            width: '75%',
+                            height: 40,
+                            borderWidth: 0,
+                            borderColor: 'black',
+                            borderRadius: 10,
+                            backgroundColor: colors.red,
+                            justifyContent: 'center',
+                            aligntItems: 'center',
+                            marginTop: '10%'
                         }}>
-                            Bad Vibes?
+                        <Text
+                            style={{
+                                fontSize: 26,
+                                textAlign: 'center',
+                                fontFamily: 'Rubik-Regular'
+                            }}>
+                            BAD VIBES
                         </Text>
-                        <Text style={{
-                            fontSize:20, 
-                            textAlign:'center', 
-                            marginTop:'10%',
-                            fontFamily: 'Rubik-Regular'
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        onPress={() => props.cancelReport()} 
+                        style={{ 
+                            width: '75%', 
+                            height: 40,
+                            borderWidth: 0,
+                            borderColor: 'black',
+                            borderRadius: 10,
+                            backgroundColor: colors.blue,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            marginTop: 10
                         }}>
-                                The sender will be temporarily banned.
+                        <Text
+                            style={{
+                                fontSize: 26,
+                                textAlign: 'center',
+                                fontFamily: 'Rubik-Regular'
+                            }}>
+                            Cancel
                         </Text>
-                        <TouchableOpacity 
-                            onPress={() => this.props.report()} 
-                            style={{ 
-                                width: '75%',
-                                height: 40,
-                                borderWidth: 0,
-                                borderColor: 'black',
-                                borderRadius: 10,
-                                backgroundColor: colors.red,
-                                justifyContent: 'center',
-                                aligntItems: 'center',
-                                marginTop: '10%'
-                            }}>
-                            <Text
-                                style={{
-                                    fontSize: 26,
-                                    textAlign: 'center',
-                                    fontFamily: 'Rubik-Regular'
-                                }}>
-                                BAD VIBES
-                            </Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity 
-                            onPress={() => this.props.cancelReport()} 
-                            style={{ 
-                                width: '75%', 
-                                height: 40,
-                                borderWidth: 0,
-                                borderColor: 'black',
-                                borderRadius: 10,
-                                backgroundColor: colors.blue,
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                marginTop: 10
-                            }}>
-                            <Text
-                                style={{
-                                    fontSize: 26,
-                                    textAlign: 'center',
-                                    fontFamily: 'Rubik-Regular'
-                                }}>
-                                Cancel
-                            </Text>
-                        </TouchableOpacity>                    
-                </View>  
-            </Modal>
-        )
-    }
+                    </TouchableOpacity>                    
+            </View>  
+        </Modal>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1, 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        marginLeft: '5%', 
+        marginRight: '5%', 
+        marginTop: '40%', 
+        marginBottom: '40%', 
+        backgroundColor: colors.cream, 
+        borderWidth: 0, 
+        borderColor: 'black', 
+        borderRadius: 10, 
+        paddingLeft: '5%', 
+        paddingRight: '5%'
+    },
+    text: {
+        fontSize: 36, 
+        textAlign: 'center', 
+        fontFamily: 'Rubik-Regular'
+    },
+    yesButton: {
+        width: '75%',
+        height: 40,
+        borderWidth: 0,
+        borderColor: 'black',
+        borderRadius: 10,
+        backgroundColor: colors.blue,
+        justifyContent: 'center',
+        marginTop: '20%',
+        alignItems: 'center'
+    },
+    yesButtonText: {
+        fontSize: 26,
+        fontFamily: 'Rubik-Regular'
+    },
+    cancelButton: { 
+        width: '75%',
+        height: 40,
+        borderWidth: 0,
+        borderColor: 'black',
+        borderRadius: 10,
+        backgroundColor: colors.red,
+        justifyContent: 'center',
+        marginTop: 10,
+        alignItems: 'center'
+    },
+    cancelButtonText: {
+        fontSize: 26,
+        fontFamily: 'Rubik-Regular'
+    }
+})
 
 const mapReduxStateToProps = reduxState => ({
     reduxState
